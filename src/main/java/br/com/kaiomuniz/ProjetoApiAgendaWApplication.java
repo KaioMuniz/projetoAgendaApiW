687d4bc55c6a12abf2af56d670fe47040eb23226
KaioMuniz <kkaioribeiro@gmail.com>
2025-06-21 11:25:30 -0300

    projeto iniciado, com end point(//todo) e swagger

---
+++ src/main/java/br/com/kaiomuniz/ProjetoApiAgendaWApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaiomuniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiAgendaWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiAgendaWApplication.class, args);
+	}
+
+}
